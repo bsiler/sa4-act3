6442ed5bb85e37352686a4754058a4e8d0bf4431
Benjamin Siler <bengsiler@gmail.com>
2024-02-07 18:47:19 -0600

    Added too low/too high hints

--- guess_number.py
+++ guess_number.py
@@ -8,5 +8,9 @@ while guess != str(number):
     if guess == 'q':
         print(f"Sorry! The number was {number}.")
         exit()
+    elif int(guess) > number:
+        print(f"Too high!")
+    else:
+        print(f"Too low!")
     guess = input("Try again: ")
 print("Congratulations! You guessed the right number.")
