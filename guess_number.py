b880082e31548637d06759a1ec6722200acbabda
Benjamin Siler <bengsiler@gmail.com>
2024-02-07 18:56:28 -0600

    Added guess limit

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,14 @@
 number = 10
-
+guesses_left = 5
 print("I'm thinking of a number...")
 guess = input("What number am I thinking of? ")
-
+guesses_left -= 1
 
 while guess != str(number):
-    if guess == 'q':
+    if guess == 'q' or guesses_left == 0:
         print(f"Sorry! The number was {number}.")
         exit()
+    print(f"You have " + str(guesses_left) + " guess(es) remaining.")
     guess = input("Try again: ")
+    guesses_left -= 1
 print("Congratulations! You guessed the right number.")
